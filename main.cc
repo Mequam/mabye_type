2bd3f0d3bb235d42e1b3196c1ddc1c2e2b3af260
mequam <blue9ja@gmail.com>
2022-09-03 23:57:17 -0400

    got the basic features of the program working :D

--- main.cc
+++ main.cc
@@ -1,2 +1,49 @@
-#include <iostream>
-#include <thread>
+#include <unistd.h>
+#include <termios.h>
+#include <poll.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+	//set up the user arguments
+	int wait_time = 1;
+	if (argc > 1) {
+		printf("%s\n",argv[1]);
+		fsync(0);
+	}
+	if (argc > 2) //display information out to the user
+		wait_time = atoi(argv[2]);
+
+
+
+	struct termios old_tio, new_tio;
+	unsigned char c;
+
+	/* get the terminal settings for stdin */
+	tcgetattr(STDIN_FILENO,&old_tio);
+
+	/* we want to keep the old setting to restore them a the end */
+	new_tio=old_tio;
+
+	/* disable canonical mode (buffered i/o) and local echo */
+	new_tio.c_lflag &=(~ICANON );//& ~ECHO);
+
+	/* set the new settings immediately */
+	tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+
+	struct pollfd poll_stdin;
+	poll_stdin.fd = STDIN_FILENO;	
+	poll_stdin.events = POLLIN;
+	
+	poll(&poll_stdin,1,wait_time*1000);
+
+
+
+	/* restore the former settings */
+	tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+
+	if ((poll_stdin.revents & POLLIN))
+		return 0;
+	return 1;
+}
